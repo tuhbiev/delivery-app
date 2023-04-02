eb91f4fee4d77d3d43bf32732f8aff7ad90c063c
radis <radis.ramisovich@gmail.com>
2023-04-02 15:53:15 +0300

    initial app

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrderTrackingComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
